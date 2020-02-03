3c56cbc83b6540181ea7b0cb4d886523da5551eb
kuldip-barot <barotkuldip8111998@gmail.com>
2020-02-03 17:53:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ with open('README.md') as f:
 setuptools.setup(
     author="Kuldeep Barot",
     #author_email="shay.palachy@gmail.com",
-    name='anncorra',
+    name="anncorra",
     license="Apache License 2.0",
-    description='Anncorra is a python package for giving meaning to POS (Part of Speech) tags.',
-    version='v0.0.1',
+    description="Anncorra is a python package for giving meaning to POS (Part of Speech) tags.",
+    version="v0.0.2",
     long_description=README,
     url='https://github.com/kuldip-barot/anncorra',
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
-    install_requires=['json'],
+    install_requires=[],
     classifiers=[
         # Trove classifiers
         # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: Apache License 2.0',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
